34159674123479b55e1a89359ff898d59cbca8cb
ggcarmi <ggcarmi@gmail.com>
2017-11-22 15:31:11 +0200

    swipe to change color and tap on button works

--- Task1BS/ViewController.m
+++ Task1BS/ViewController.m
@@ -10,10 +10,11 @@
 
 @interface ViewController ()
 
+
 @property (weak, nonatomic) IBOutlet UIImageView *liveCommentImageView;
 - (IBAction)liveCommentButtonWasClicked:(id)sender;
-//- (IBAction)swipeToChengeLiveCommentButton:(id)sender;
-- (IBAction)swipeToChangeLiveCommentButton:(id)sender;
+- (IBAction)swipeToChangeLiveCommentButtonColor:(id)sender;
+
 
 @end
 
@@ -31,12 +32,12 @@
 }
 
 
-- (IBAction)liveCommentButtonWasClicked:(id)sender {
-    NSLog(@"live comment button was clicked");
-    
-//    self.playButton.hidden = YES;
-//    [self.videoPlayer play];
-}
+//- (IBAction)liveCommentButtonWasClicked:(id)sender {
+//    NSLog(@"live comment button was clicked");
+//
+////    self.playButton.hidden = YES;
+////    [self.videoPlayer play];
+//}
 
 //- (IBAction)swipeToChengeLiveCommentButton:(id)sender {
 //    self.liveCommentImageView.image = [UIImage imageNamed:@"button_livecomment_white"];
@@ -44,8 +45,19 @@
 //
 //}
 
-- (IBAction)swipeToChangeLiveCommentButton:(id)sender {
-        self.liveCommentImageView.image = [UIImage imageNamed:@"button_livecomment_white"];
+//- (IBAction)swipeToChangeLiveCommentButton:(id)sender {
+//        self.liveCommentImageView.image = [UIImage imageNamed:@"button_livecomment_white"];
+//}
+
+
+
+
+- (IBAction)liveCommentButtonWasClicked:(id)sender {
+    NSLog(@" $$ live comment button was clicked");
+}
+
+- (IBAction)swipeToChangeLiveCommentButtonColor:(id)sender {
+    NSLog(@" swipe live comment button was swiped");
 
 }
 @end
